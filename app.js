d15dafe2ada97829b09025f208181fc4c92863b8
sergiomendez281 <160558709+sergiomendez281@users.noreply.github.com>
2024-12-02 14:58:29 -0600

    version final juego numero secreto

---
+++ app.js
@@ -0,0 +1,65 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }else{
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'ya se sortearon todos los numeros posibles');
+    }else{
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        }else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function generarCondicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número seceto');
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    generarCondicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+generarCondicionesIniciales();
